82dc6e07c4be62419da5ea6879cae8bcbcd14aec
Fernando <fernandobarcalarodriguez@gmail.com>
2022-07-27 12:55:54 +0200

    add about me section

--- src/components/Sections/AboutMe/AboutMe.tsx
+++ src/components/Sections/AboutMe/AboutMe.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import myPicture from '../../../assets/me.jpg';
+import classes from './AboutMe.module.css';
 
 const AboutMe = () => {
     return (
-        <>
-            AboutMe placeholder
-        </>
+        <div className={classes['about-me-container']}>
+            <img src={myPicture} alt='me' ></img>
+            <p>
+                I’m a Software Developer, working as <b>Fullstack</b> and interested in fields like <b>Software Design</b> or <b>Game Development</b>.
+                <br/>
+                <br/>
+                In my free time, besides doing sports or playing videogames, I also like to learn and try new techs and concepts like <b>React</b>, <b>Software Architecture</b> or <b>Unity</b>.
+            </p>
+        </div>
     );
 }
 
