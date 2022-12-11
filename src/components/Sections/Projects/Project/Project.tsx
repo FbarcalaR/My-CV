17e0b5a1d56c315343a0bbd411f7029219dd97d2
Fernando Barcala Rodriguez <fbarcalarodrigez@vector-software.com>
2022-12-11 23:12:39 +0100

    responsive styles and warnings

--- src/components/Sections/Projects/Project/Project.tsx
+++ src/components/Sections/Projects/Project/Project.tsx
@@ -9,20 +9,20 @@ interface Props {
 }
 
 const Project: FunctionComponent<Props> = (props) => {
-        return (
-            <div className={classes['project-container']}>
-                <div className={classes['project-header-container']}>
-                    <a href={props.repositoryLink} target='_blank' >
-                        <InsertLinkIcon className={classes['link-icon']} />
-                    </a>
-                    <span className={`${classes['project-title']} font-body`}>{props.title}</span>
-                </div>
-
-                <p className={`${classes['project-description']} font-body`}>
-                    {props.children}
-                </p>
+    return (
+        <div className={classes['project-container']}>
+            <div className={classes['project-header-container']}>
+                <a href={props.repositoryLink} target='_blank' rel='noreferrer' >
+                    <InsertLinkIcon className={classes['link-icon']} />
+                </a>
+                <span className={`${classes['project-title']} font-body`}>{props.title}</span>
             </div>
-        );
+
+            <p className={`${classes['project-description']} font-body`}>
+                {props.children}
+            </p>
+        </div>
+    );
 }
 
 export default Project;
