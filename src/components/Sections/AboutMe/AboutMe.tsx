a90da2ff7b3a37386f529c0be43eaa58a4815183
FernandoYamdu <fernando@yamdu.com>
2025-12-02 09:16:01 +0100

    update about me

--- src/components/Sections/AboutMe/AboutMe.tsx
+++ src/components/Sections/AboutMe/AboutMe.tsx
@@ -26,14 +26,12 @@ const AboutMe = () => {
         <div ref={containerDiv} className={classes['about-me-container']}>
             <img ref={imageElement} src={myPicture} alt='me' className={`${classes['shift-effect']} ${classes['from-left']}`} ></img>
             <p ref={bodyDiv} className={`font-body ${classes['shift-effect']} ${classes['from-right']}`}>
-                Software Engineer with 5+ years of experience web and mobile applications using .NET, React, Angular, SQL, 
-                and modern system architectures and principles (microservices, DDD, CI/CD). Passionate about software 
-                architecture, clean code, and having a look at new AI/ML technologies.
+                Software Engineer with 6+ years of experience developing web and mobile applications using .NET, React, Angular, SQL, 
+                and modern system architectures and principles like microservices, DDD or CI/CD.
                 <br/>
                 <br/>
                 Collaborative, fast learner and invested in continuous improvement, willing to coach and learn from others while having fun.
-                I like to create quality and readable code, always ready to learn new concepts about Software Design and Architecture,
-                and engaged in continuous improvement and professional development.
+                Passionate about software architecture, clean code, and having a look at new AI technologies.
                 <br/>
                 <br/>
                 In my free time, apart of doing sports, making (neapolitan) pizza or playing my guitar, I also like to learn about and try new techs and concepts like
